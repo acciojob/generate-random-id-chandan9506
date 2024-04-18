dbae4f49e9d780577bebb24673f54b48eb7de9c9
chandan9506 <152258839+chandan9506@users.noreply.github.com>
2024-04-18 15:30:03 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,7 +1,15 @@
 function makeid(l) {
   // write your code here
+	let char = "1234567890QWERTYUIOPLKJHGFDSAZXCVBNMqwertyuiopasdfghjklzxcvbnm";
+	let randomString = "";
+	for (let i=0; i<l; i++) {
+		let randomNum = Math.floor(Math.random()*char.length);
+		randomString += char.substring(randomNum,randomNum+1);
+	}
+	return randomString;
 }
 
 // Do not change the code below.
+
 const l = prompt("Enter a number.");
 alert(makeid(l));
